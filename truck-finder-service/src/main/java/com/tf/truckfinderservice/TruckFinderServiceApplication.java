9ff3504bac1bdea5efba167a26d328f4f2e44b00
apatiban <aruna.patibandla@gmail.com>
2022-04-24 00:12:47 -0400

    First Commit

---
+++ truck-finder-service/src/main/java/com/tf/truckfinderservice/TruckFinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tf.truckfinderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckFinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckFinderServiceApplication.class, args);
+	}
+
+}
